9277f88d4c0d1786df7b8bf7621aa6ddfa47a382
RafalMederakWSEI <rafal.mederak@microsoft.wsei.edu.pl>
2022-07-01 11:49:55 +0200

    GamesList Page - Added the game update functionality.

--- GamesApp/Pages/GamesList.xaml.cs
+++ GamesApp/Pages/GamesList.xaml.cs
@@ -29,5 +29,58 @@ namespace GamesApp.Pages
 
             gridGames.ItemsSource = docs.ToList();
         }
+
+        private void gridGames_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(gridGames.SelectedIndex >= 0)
+            {
+                if (gridGames.SelectedItems.Count >= 0)
+                {
+                    if (gridGames.SelectedItems[0].GetType() == typeof(Gra))
+                    {
+                        Gra g = (Gra)gridGames.SelectedItems[0];
+                        txtGameIdUpdate.Text = Convert.ToString(g.id_gry);
+                        txtGameNameUpdate.Text = g.nazwa_gry;
+                        txtGameProducentUpdate.Text = g.producent;
+                        txtGameAgeCategoryUpdate.Text = g.kategoria_wiekowa_PEGI;
+                        txtGameTypeUpdate.Text = g.typ_gry;
+                        txtGamePriceUpdate.Text = Convert.ToString(g.cena);
+                        txtGamePlatformUpdate.Text = g.platforma;
+                        updatingDoctorID = g.id_gry;
+                    }
+                }
+            }               
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            WypozyczalniaGierEntities db = new WypozyczalniaGierEntities();
+
+            var result = from g in db.Gras where g.id_gry == updatingDoctorID select g;
+
+            Gra game = result.SingleOrDefault();
+
+            if(game != null)
+            {
+                game.id_gry = Convert.ToInt32(txtGameIdUpdate.Text);
+                game.nazwa_gry = txtGameNameUpdate.Text;
+                game.producent = txtGameProducentUpdate.Text;
+                game.kategoria_wiekowa_PEGI = txtGameAgeCategoryUpdate.Text;
+                game.typ_gry = txtGameTypeUpdate.Text;
+                game.cena = Convert.ToDecimal(txtGamePriceUpdate.Text);
+                game.platforma = txtGamePlatformUpdate.Text;
+            }
+
+            db.SaveChanges();
+            LoadGames();
+        }
+
+        private int updatingDoctorID = 0;
+
+        private void LoadGames()
+        {
+            WypozyczalniaGierEntities db = new WypozyczalniaGierEntities();
+            gridGames.ItemsSource = db.Gras.ToList();
+        }
     }
 }
